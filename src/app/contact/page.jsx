2e559f64805a53ebaea5439a4c1b1bc0678a03f3
Yuri Castilho <yuricastilho2005@gmail.com>
2023-09-30 09:57:35 -0300

    style: espacamento entre linkedin e github

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -16,31 +16,40 @@ const Contact = () => {
       <div className={styles.member}>
         <Image className={styles.image} src={Ana}/>
         <h3>Ana Beatriz Martins</h3>
+      <div className={styles.div}>
         <a href='https://www.linkedin.com/in/anabe-sc/'>
+          <Image className={styles.social} src={Linkedin}/>
         </a>
         <a href='https://github.com/anabmartins'>
           <Image className={styles.social} src={Github}/>
         </a>
+        </div>
       </div>
 
       <div className={styles.member}>
         <Image className={styles.image} src={Yuri}/>
         <h3>Yuri Castilho</h3>
+        <div className={styles.div}>
         <a href='https://www.linkedin.com/in/yuri-castilhoo/'>
+        <Image className={styles.social} src={Linkedin}/>
         </a>
         <a href='https://github.com/YuriCast'>
           <Image className={styles.social} src={Github}/>
         </a>
       </div>
+      </div>
 
       <div className={styles.member}>
         <Image className={styles.image} src={Isa}/>
         <h3>Isadora Wenzel</h3>
+        <div className={styles.div}>
         <a href='https://www.linkedin.com/in/isadora-ws/'>
+          <Image className={styles.social} src={Linkedin}/>
         </a>
         <a href='https://github.com/isaws06'>
           <Image className={styles.social} src={Github}/>
         </a>
+        </div>
         </div>
 
     </div>
