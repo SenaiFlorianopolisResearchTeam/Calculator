afe704481448b47eaed3481259d664bb1567ccb7
Anabe <99266498+anabmartins@users.noreply.github.com>
2023-10-16 13:50:00 -0300

    Update page.jsx

--- src/app/chapters/[id]/page.jsx
+++ src/app/chapters/[id]/page.jsx
@@ -1,4 +1,3 @@
-
 "use client"
 import React from "react";
 import { useRouter } from "next/navigation";
@@ -17,6 +16,7 @@ const ChapterPage = ({ params }) => {
 
   return (
     <>
+      <p>{params.title}</p>
     </>
   );
 };
