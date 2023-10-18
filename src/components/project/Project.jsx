e16174763ac29dfbcbb0da130120e1524aa8f954
anabmartins <ana_beatriz-martins@estudante.sesisenai.org.br>
2023-10-18 12:55:10 -0300

    adding a new Project component on about page

--- src/components/project/Project.jsx
+++ src/components/project/Project.jsx
@@ -3,9 +3,9 @@ import styles from './project.module.css'
 export default function Project() {
     return (
         <>
-                <h1 className={styles.title}>Grupos</h1>
+            <h1 className={styles.title}>Grupos</h1>
             <div className={styles.containerProject}>
-                
+
                 <ol className={styles.text}>
                     <li>Desenvolvimento de uma plataforma calculadora para análise dos níveis de ruído rodoviário</li>
                     <p> (Ana Beatriz Martins, Yuri Castilho e Isadora Wenzel da Silveira)</p>
@@ -18,13 +18,21 @@ export default function Project() {
                     <li>Análise do isolamento do ruído de impacto entre pisos</li>
                     <p>(Bibiana Engel Ribeiro, Carolina Vasco da Silva e Larissa Miranda Medeiros)</p>
                 </ol>
-            </div>
-                <h2>Nosso Projeto:</h2>
-            <div className={styles.ourProject}>
-                <p>Desenvolvimento de uma plataforma calculadora para análise dos níveis de ruído rodoviário|</p>
-                <div className={styles.pdf}>
-                    <p>Leia nosso artigo científico</p>
-                    <button>PDF</button>
+                <h2 className={styles.subtitle}>Nosso Projeto</h2>
+                <div className={styles.cardProject}>
+                    <p className={styles.titleProject}>Desenvolvimento de uma plataforma calculadora para análise dos níveis de ruído rodoviário</p>
+                    <p className={styles.textProject}>Essa plataforma se propõe a ser uma ferramenta eficaz para avaliar e mitigar os efeitos sonoros decorrentes do fluxo rodoviário. A metodologia empregada inclui a integração de dados geoespaciais, informações sobre as características das rodovias, dados de tráfego de veículos e modelos analíticos para permitir um cálculo mais preciso dos níveis de ruído em decibéis (dB) em diversos pontos ao longo das vias. Cabe ressaltar que o combate ao ruído rodoviário é um desafio global que demanda abordagens complexas e o uso de novas tecnologias para minimizar seus impactos negativos. A diversidade desses modelos e a apresentação dos conteúdos em pauta representam uma abordagem abrangente para enfrentar o desafio em questão, tornando possível a adaptação às necessidades específicas de diferentes situações e regiões e contribuindo para uma compreensão mais ampla e eficaz do tema. <span>Leia nosso artigo científico!</span>
+                    </p>
+                    <div className={styles.moreInfo}>
+                        <a href='https://github.com/anabmartins/IC-Website-2023' target='_blank'>
+                            <img src="./mdi_github.svg" />
+                            Ver repositório</a>
+                        <a className={styles.btnPDF}
+                        href='./ArtigoIC.pdf'
+                        download='Artigo.pdf'
+                        >
+                            <p id='btnTxt'>Baixar PDF</p></a>
+                    </div>
                 </div>
             </div>
         </>
